d840f959a5255ac1246b65e4200bbe63b50d07a2
Fikri Amrullah <152844321+massiki@users.noreply.github.com>
2026-01-03 19:45:41 +0700

    UI/footer (#3)
    * ui/movie-list
    
    * ui/footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="border-t border-border bg-secondary-foreground py-8">
+      <div className="container text-center">
+        <p className="text-sm text-muted-foreground">
+          © {new Date().getFullYear()} MovieF by Fikri Amrullah. Made with ❤️ and code.
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
