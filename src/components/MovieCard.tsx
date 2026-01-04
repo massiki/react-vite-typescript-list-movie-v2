944ec012d95bbc693f69bd833664b86053c8e7e2
Fikri Amrullah <152844321+massiki@users.noreply.github.com>
2026-01-04 12:14:02 +0700

    Feat/read movie list (#7)
    * setup-api
    
    * feat/read-movie-list

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,19 +1,22 @@
-import { Calendar, Star } from "lucide-react"
+import { Calendar, Star, TrendingUp } from "lucide-react"
 import { Link } from "react-router"
 
-const MovieCard = () => {
+import type { movieType } from "../types/movieType"
+
+const pathUrl = import.meta.env.VITE_PATH_URL
+
+const MovieCard = ({ movie }: { movie: movieType }) => {
   return (
     <Link
-      // to={`/movie/${movie.id}`}
-      to={`/movie/1`}
+      to={`/movie/${movie.id}`}
       className="group block"
     >
       <div className="relative overflow-hidden rounded-3xl bg-card shadow-md transition-all duration-300 hover:shadow-xl hover:-translate-y-2">
         {/* Poster Image */}
         <div className="aspect-2/3 overflow-hidden bg-muted">
           <img
-            src='https://thumbor.prod.vidiocdn.com/dBQbr6Yp29QnXIyK-MXYwSitybw=/filters:quality(70)/vidio-media-production/uploads/image/source/21790/b4895e.jpg'
-            alt='movie title'
+            src={`${pathUrl}/${movie.poster_path}`}
+            alt={movie.title}
             className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-110"
             loading="lazy"
           />
@@ -22,38 +25,32 @@ const MovieCard = () => {
         {/* Rating Badge */}
         <div className="absolute right-2 top-2 flex items-center gap-1 rounded-full bg-primary px-2 py-1 text-xs font-semibold text-primary-foreground shadow-lg">
           <Star className="h-3 w-3 fill-current" />
-          {/* <span>{movie.vote_average.toFixed(1)}</span> */}
-          <span>8.5</span>
+          <span>{movie.vote_average.toFixed(1)}</span>
         </div>
 
         {/* Content */}
         <div className="p-4">
           <h3 className="text-start mb-2 line-clamp-1 text-base font-semibold text-card-foreground transition-colors group-hover:text-primary">
-            {/* {movie.title} */}
-            Naruto
+            {movie.title}
           </h3>
 
           <div className="mb-2 flex items-center gap-2 text-xs text-muted-foreground">
             <Calendar className="h-3 w-3" />
-            {/* <span>{releaseYear}</span> */}
-            <span>2009</span>
+            <span>
+              {new Date(movie.release_date).toLocaleDateString("en-EN", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}
+            </span>
           </div>
 
-          {/* Genres */}
+          {/* Popularity */}
           <div className="flex flex-wrap gap-1">
-            {/* {genres.map((genre) => (
-              <span
-                key={genre}
-                className="rounded-full bg-accent px-2 py-0.5 text-xs text-accent-foreground"
-              >
-                {genre}
-              </span>
-            ))} */}
-
+            <TrendingUp className="h-5 w-5" />
             <span
-              className="rounded-full bg-accent px-2 py-0.5 text-xs text-accent-foreground"
-            >
-              Dewasa
+              className="rounded-full bg-accent px-2 py-0.5 text-xs text-accent-foreground"            >
+              {movie.popularity}
             </span>
           </div>
         </div>
@@ -65,7 +62,7 @@ const MovieCard = () => {
           </span>
         </div>
       </div>
-    </Link>
+    </Link >
   )
 }
 
