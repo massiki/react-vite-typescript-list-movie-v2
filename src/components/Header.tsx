92c580f1d251c50c8f47ed8806cf833f3ea582b7
massiki <fikri.amrulloh15@gmail.com>
2026-01-20 22:44:47 +0700

    add/nav-link-series-tv

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,23 @@ import { Link } from "react-router";
 import logo from '../../public/logo-with-text-movief.png'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
-import { ChevronDown, Menu } from "lucide-react";
+import { ChevronDown, Film, Menu, Tv } from "lucide-react";
 import { Sheet, SheetClose, SheetContent, SheetTrigger } from "./ui/sheet";
 import { useState } from "react";
+
+const movieLinks = [
+  { title: 'Hot', href: '/' },
+  { title: 'Popular', href: '/movie/popular' },
+  { title: 'Top Rated', href: '/movie/top-rated' },
+  { title: 'Upcomming', href: '/movie/upcomming' },
+]
+
+const seriesTvLinks = [
+  { title: 'Airing Today', href: '/' },
+  { title: 'Airing Soon', href: '/' },
+  { title: 'Popular', href: '/' },
+  { title: 'Top Rated', href: '/' },
+]
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
@@ -17,46 +31,49 @@ const Header = () => {
         </Link>
 
         {/* Desktop Navigation */}
-        <nav className="hidden items-center gap-6 md:flex">
-          <Link
-            to="/"
-            className="text-sm font-medium transition-colors hover:text-primary"
-          >
-            Home
-          </Link>
-
+        <nav className="hidden items-center gap-1 md:flex">
+          {/* movies */}
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="flex items-center gap-1 text-sm font-medium">
+              <Button variant="ghost" className="flex items-center gap-1 text-sm font-medium cursor-pointer">
+                <Film />
                 Movies
                 <ChevronDown className="h-4 w-4" />
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end" className="w-40 bg-popover">
-              <DropdownMenuItem asChild>
-                <Link
-                  to='/movie/popular'
-                  className="w-full cursor-pointer"
-                >
-                  Popular
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem asChild>
-                <Link
-                  to='/movie/top-rated'
-                  className="w-full cursor-pointer"
-                >
-                  Top Rated
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem asChild>
-                <Link
-                  to='/movie/upcomming'
-                  className="w-full cursor-pointer"
-                >
-                  Upcomming
-                </Link>
-              </DropdownMenuItem>
+              {movieLinks.map((link, index) => (
+                <DropdownMenuItem asChild>
+                  <Link key={index}
+                    to={`${link.href}`}
+                    className="w-full cursor-pointer"
+                  >
+                    {link.title}
+                  </Link>
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+          {/* series tv */}
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="flex items-center gap-1 text-sm font-medium cursor-pointer">
+                <Tv />
+                Series TV
+                <ChevronDown className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-40 bg-popover">
+              {seriesTvLinks.map((link, index) => (
+                <DropdownMenuItem key={index} asChild>
+                  <Link
+                    to={link.href}
+                    className="w-full cursor-pointer"
+                  >
+                    {link.title}
+                  </Link>
+                </DropdownMenuItem>
+              ))}
             </DropdownMenuContent>
           </DropdownMenu>
         </nav>
@@ -65,53 +82,47 @@ const Header = () => {
         <Sheet open={isOpen} onOpenChange={setIsOpen}>
           <SheetTrigger asChild className="md:hidden">
             <Button variant="ghost" size="icon">
-              <Menu className="h-6 w-6" />
+              <Menu className="h-8 w-8" />
               <span className="sr-only">Toggle menu</span>
             </Button>
           </SheetTrigger>
           <SheetContent side="right" className="w-[280px] bg-background">
-            <div className="flex flex-col gap-6 pt-6">
+            <div className="flex flex-col gap-6 p-6">
               <div className="flex items-center justify-between">
                 <span className="text-lg font-bold">Menu</span>
               </div>
 
-              <nav className="flex flex-col gap-4">
-                <SheetClose asChild>
-                  <Link
-                    to="/"
-                    className="text-base font-medium transition-colors hover:text-primary"
-                  >
-                    Home
-                  </Link>
-                </SheetClose>
-
+              <nav className="flex flex-col gap-1">
+                {/* movies */}
                 <div className="space-y-3">
                   <span className="text-sm font-semibold text-muted-foreground">Movies</span>
-                  <div className="flex flex-col gap-2 pl-3">
-                    <SheetClose asChild>
-                      <Link
-                        to='/movie/popular'
-                        className="text-sm font-medium transition-colors hover:text-primary"
-                      >
-                        Popular
-                      </Link>
-                    </SheetClose>
-                    <SheetClose asChild>
-                      <Link
-                        to='/movie/top-rated'
-                        className="text-sm font-medium transition-colors hover:text-primary"
-                      >
-                        Top Rated
-                      </Link>
-                    </SheetClose>
-                    <SheetClose asChild>
-                      <Link
-                        to='/movie/upcomming'
-                        className="text-sm font-medium transition-colors hover:text-primary"
-                      >
-                        Upcomming
-                      </Link>
-                    </SheetClose>
+                  <div className="flex flex-col gap-2 pl-3 mt-3">
+                    {movieLinks.map((link, index) => (
+                      <SheetClose key={index} asChild>
+                        <Link
+                          to={`${link.href}`}
+                          className="text-sm font-medium transition-colors hover:text-primary"
+                        >
+                          {link.title}
+                        </Link>
+                      </SheetClose>
+                    ))}
+                  </div>
+                </div>
+                {/* series tv */}
+                <div className="space-y-3">
+                  <span className="text-sm font-semibold text-muted-foreground">Series TV</span>
+                  <div className="flex flex-col gap-2 pl-3 mt-3">
+                    {seriesTvLinks.map((link, index) => (
+                      <SheetClose asChild>
+                        <Link key={index}
+                          to={link.href}
+                          className="text-sm font-medium transition-colors hover:text-primary"
+                        >
+                          {link.title}
+                        </Link>
+                      </SheetClose>
+                    ))}
                   </div>
                 </div>
               </nav>
